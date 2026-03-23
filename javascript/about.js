2301f69e92aade05a6603aedd553dabca619dab2
Chris Rios <robotman40@protonmail.com>
2026-03-22 23:34:51 -0700

    Update About page to use MacOS icon for MacOS systems

--- javascript/about.js
+++ javascript/about.js
@@ -1,7 +1,12 @@
 const { ipcRenderer } = require('electron');
+const { platform } = require('os');
 
 window.onload = function() {
     ipcRenderer.invoke('get-version').then((version) => {
         document.getElementById('version').textContent = version;
     });
+
+    if (platform() === 'darwin') {
+        document.getElementById('app-icon').src = "assets/electerm_logo_macos.png";
+    }
 }
