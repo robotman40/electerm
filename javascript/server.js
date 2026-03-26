6516b525ee770db5ee3fe20ce49c0e50001d2b27
Chris Rios <robotman40@protonmail.com>
2026-03-26 12:14:29 -0700

    Improve comments in server.js

--- javascript/server.js
+++ javascript/server.js
@@ -2,12 +2,16 @@ const { spawn } = require('node-pty');
 const os = require('os');
 
 class PTYSession {
+    // Session class that serves as an interface with PTY session
+
     constructor(rows, cols, window) {
+        // Try starting a PTY session
         try {
             // On Windows, cmd.exe must be used since powershell.exe has rendering problems. On Unix-like systems, use the default shell.
             const plat = os.platform();
             const shell = plat === 'win32' ? 'cmd.exe' : process.env.SHELL || '/bin/sh' ; 
 
+            // Spawn the default shell
             this.ptyProcess = spawn(shell, [], {
                 cwd: process.env.HOME,
                 env: process.env,
@@ -16,30 +20,36 @@ class PTYSession {
                 cols: cols
             }); 
 
+            // Send data when new data is available from the pTY
             this.ptyProcess.on('data', (data) => {
                 window.webContents.send('send-data-to-term', data)
             });
 
+            // Close the window if the PTY session ends
             this.ptyProcess.on('exit', (code, signal) => {
                 console.log(`The shell excited with code ${code} and signal ${signal}`);
                 window.close();
             })
 
         } catch (e) {
+            // Throw an error if PTY session creation fails
             console.log(e)
             return e;
         }
     }
 
     resizePTY(rows, cols) {
+        // Handles resizing the PTY
         this.ptyProcess.resize(cols, rows);
     }
 
     writeToPTY(data) {
+        // Writes to the PTY
         this.ptyProcess.write(data);
     }
 
     destroy() {
+        // Kills the PTY session
         this.ptyProcess.kill();
     }
 }
