42119ffc74e65760889f19a1e0619f61b62eaf19
Chris Rios <robotman40@protonmail.com>
2026-03-26 12:09:03 -0700

    Refactor about.js to be more efficient

--- javascript/about.js
+++ javascript/about.js
@@ -1,22 +1,11 @@
-async function getVersion() {
-    return await window.app.getVersion();
-}
-
-async function getOS() {
-    return await window.app.getOS();
-}
-
-window.onload = function() {
-     getVersion().then(version => {
-        document.getElementById('version').textContent = version;
-     });
+window.onload = async function() {
+     document.getElementById('version').textContent = await window.app.getVersion();
 
     const iconImage = document.getElementById('app-icon');
-    getOS().then(os => {
-        if (os === 'darwin') {
-            iconImage.src = "assets/electerm_logo_macos.png";
-        } else {
-            iconImage.src = "assets/electerm_logo.png";
-        }
-    });
+    const os = await window.app.getOS();
+    if (os === 'darwin') {
+        iconImage.src = "assets/electerm_logo_macos.png";
+    } else {
+        iconImage.src = "assets/electerm_logo.png";
+    }
 }
